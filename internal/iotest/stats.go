10453ae3e0f37da6774a7d357a1ea89e62d7020e
jesse galley <jesse.galley@gmail.com>
2025-06-11 06:49:21 -0300

    fmt, more iotest skeleton

---
+++ internal/iotest/stats.go
@@ -0,0 +1,19 @@
+package iotest 
+
+
+type Stats struct {
+
+}
+
+
+
+type OpStats struct {
+  Op string  
+  Count int64
+  Bytes int64  
+  LatMin float64
+  LatAvg float64
+  LatMax float64
+}
+
+
